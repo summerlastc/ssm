1728b96aa249fb48a19da36af4d892795f6d2868
chuz2 <1663701826@qq.com>
2019-06-21 21:39:04 +0800

    2019/6/21

---
+++ itcast-ssm-service/src/main/java/com/itheima/service/Impl/IPermissionServiceImpl.java
@@ -0,0 +1,25 @@
+package com.itheima.service.Impl;
+
+import com.itheima.dao.IPermissionDao;
+import com.itheima.domain.Permission;
+import com.itheima.service.IPermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class IPermissionServiceImpl implements IPermissionService {
+
+    @Autowired
+    private IPermissionDao permissionDao;
+
+    @Override
+    public void save(Permission permission) throws Exception{
+        permissionDao.save(permission);
+    }
+
+    @Override
+    public List<Permission> findAll() throws Exception{
+        return permissionDao.findAll();
+    }
+}
